983c52242395dcc0876aa80ae6a1d7a560867975
SanthoshRam98 <santhoshram.office@gmail.com>
2025-06-20 22:07:09 +0800

    WIP: Power on VM changes

--- app/models/manageiq/providers/nutanix/infra_manager.rb
+++ app/models/manageiq/providers/nutanix/infra_manager.rb
@@ -1,9 +1,5 @@
 class ManageIQ::Providers::Nutanix::InfraManager < ManageIQ::Providers::InfraManager
   supports :create
-<<<<<<< HEAD
-  supports :management_console
-=======
->>>>>>> b9c0271 (Added hostname validation for nutanix provider)
   validate :hostname_uniqueness_valid?
   def allow_targeted_refresh?
     true
